1bb1db626b8fe5b837053c6e7074297be2d76fd2
Pan Li <incarnation.p.lee@outlook.com>
2022-08-16 11:08:30 +0800

    Add options for tool.
    Signed-off-by: Pan Li <incarnation.p.lee@outlook.com>

---
+++ internal/options/options.go
@@ -0,0 +1,55 @@
+package options
+
+import (
+	"flag"
+)
+
+type Option struct {
+	Key         string
+	Val         string
+	Description string
+}
+
+type Options struct {
+	Options []Option
+}
+
+const (
+	optionDecode       = "decode"
+
+	invalidOptionValue = "invalid option value"
+)
+
+var supportedOptions = []Option{
+	Option{
+		Key: optionDecode,
+		Val: invalidOptionValue,
+		Description: "decode the hex/bin RISC-V instruction to asm code",
+	},
+}
+
+func CreateOptions() Options {
+	options := Options{
+		Options: []Option{},
+	}
+
+	values := make([]string, len(supportedOptions))
+
+	for i, v := range supportedOptions {
+		flag.StringVar(&values[i], v.Key, v.Val, v.Description)
+	}
+
+	flag.Parse()
+
+	for i, v := range values {
+		if v != invalidOptionValue {
+			options.Options = append(options.Options, Option{
+				Key: supportedOptions[i].Key,
+				Val: v,
+				Description: supportedOptions[i].Description,
+			})
+		}
+	}
+
+	return options
+}
---
+++ internal/options/options_test.go
@@ -0,0 +1,17 @@
+package options
+
+import (
+	"os"
+	"github.com/Incarnation-p-lee/cachalot/pkg/assert"
+	"testing"
+)
+
+func TestCreateOptions(t *testing.T) {
+	os.Args = append(os.Args, "-decode")
+	os.Args = append(os.Args, "0x1111")
+	testOptions := CreateOptions()
+
+	assert.IsNotNil(t, testOptions.Options, "options should not be nil")
+	assert.IsEqual(t, 1, len(testOptions.Options), "no arguments should have emtpy options")
+}
+
