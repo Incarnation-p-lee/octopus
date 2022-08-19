d208adee465fb8fc2ae971dd0c6c748da71b4651
Pan Li <incarnation.p.lee@outlook.com>
2022-08-19 08:53:24 +0800

    Add asm module.
    * Add instruction struct with metada
    * Add getOpcode, getFunct3 and getFunct7.
    
    Signed-off-by: Pan Li <incarnation.p.lee@outlook.com>

---
+++ internal/asm/asm.go
@@ -0,0 +1,54 @@
+package asm
+
+type RISCVInstruction struct {
+	EncodedBinary uint32
+	MetaData      RISCVInstructionMetaData
+}
+
+type RISCVInstructionMetaData struct {
+	Opcode            int
+	InstructionClass  string
+	InstructionFormat string
+	InstructionName   string
+	Funct3ToMetaData  map[int]RISCVInstructionMetaData
+	Funct7ToMetaData  map[int]RISCVInstructionMetaData
+}
+
+const (
+	ISAClassRV32I = "RV32I"
+
+	ISAFormatRegister = "Register(R)"
+	ISAFormatLoad = "Load(I)"
+	ISAFormatStore = "Store(S)"
+	ISAFormatConditionalJump = "ConditionalJump(B)"
+	ISAFormatLongImmediate = "LongImmediate(U)"
+	ISAFormatUnconditionalJump = "UnconditionalJump(J)"
+
+	unknownInstructionName = "unknown instruction name"
+	invalidInstructionValue = -1
+
+	opcodeOffset = 0
+	opcodeMask = uint32(0x7f) << opcodeOffset
+
+	funct3Offset = 12
+	funct3Mask = uint32(0x7) << funct3Offset
+
+	funct7Offset = 25
+	funct7Mask = uint32(0x7f) << funct7Offset
+)
+
+func getBitMask(binary uint32, mask, offset uint32) uint32 {
+	return (binary & mask) >> offset
+}
+
+func (instr *RISCVInstruction) getOpcode() uint32 {
+	return getBitMask(instr.EncodedBinary, opcodeMask, opcodeOffset)
+}
+
+func (instr *RISCVInstruction) getFunct3() uint32 {
+	return getBitMask(instr.EncodedBinary, funct3Mask, funct3Offset)
+}
+
+func (instr *RISCVInstruction) getFunct7() uint32 {
+	return getBitMask(instr.EncodedBinary, funct7Mask, funct7Offset)
+}
---
+++ internal/asm/go.mod
@@ -0,0 +1,5 @@
+module internal/asm
+
+go 1.19
+
+require github.com/Incarnation-p-lee/cachalot/pkg/assert v0.0.0-20220801030122-dd268c84b475
